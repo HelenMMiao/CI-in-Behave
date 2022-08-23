dc4ef61bccebfde9f937aa655035026b650702a4
HelenM <ceiba_miao@hotmail.com>
2022-08-24 10:34:52 +1200

    Add Cucumber Json coverter

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,18 +1,30 @@
 from behave import *
 
-@given(u'have number 9')
-def step_impl(context):
-    context.firstNumber = 9
+@given(u'have number {number:d}')
+def step_impl(context, number):
+    context.firstNumber = number
+
+@given(u'any first number is {number:d}')
+def step_impl(context, number):
+
+    context.firstNumber = number
+
+@when(u'another number {number:d}')
+def step_impl(context, number):
+    context.secondNumber = number
+    context.actualResult = context.firstNumber + context.secondNumber
+
+@when(u'add {number:d}')
+def step_impl(context, number):
+    print("********before print****************")
+    print(type(number))
+    print("********after print****************")
+    context.secondNumber = number
+    context.actualResult = context.firstNumber + context.secondNumber
+
+@then(u'result {result:d}')
+def step_impl(context, result):
+    assert (context.actualResult == result)
 
 
-@when(u'take away 4')
-def step_impl(context):
-    context.secondNumber = 4
-    context.actualResult = 9-4
-
-
-@then(u'result is 5')
-def step_impl(context):
-    context.expected = 5
-    assert (context.expected == context.actualResult)
 
